3bbf44c8d06e5ff52c3477bcc97743552a3c1ea4
takasqr <95556025+takasqr@users.noreply.github.com>
2023-05-24 23:35:40 +0900

    GTMのイベントを開発環境では実行しないように

--- src/Analytics.ts
+++ src/Analytics.ts
@@ -4,10 +4,14 @@ class Analytics {
 
     useWebApp(): void {
         if (this.isStartedWebApp === false) {
-            
-            // Google TagManager
-            // @ts-ignore
-            dataLayer.push({ event: 'use-webapp' })
+
+            if (process.env.NODE_ENV === 'production') {
+                // Google TagManager
+                // @ts-ignore
+                dataLayer.push({ event: 'use-webapp' })
+            } else {
+                console.log('開発環境：use-webapp が実行されました。')
+            }
 
             this.isStartedWebApp = true
         }
@@ -16,9 +20,13 @@ class Analytics {
     useFunction(functionName: String): void {
         if (!this.usedFunctions.includes(functionName)) {
 
-            // Google TagManager
-            // @ts-ignore
-            dataLayer.push({ event: 'use-function' })
+            if (process.env.NODE_ENV === 'production') {
+                // Google TagManager
+                // @ts-ignore
+                dataLayer.push({ event: 'use-function' })
+            } else {
+                console.log('開発環境：use-function が実行されました。')
+            }
 
             this.usedFunctions.push(functionName)
         }
